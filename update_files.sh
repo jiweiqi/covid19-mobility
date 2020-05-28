6240051e62a25fb1af280d31b2dcb750f97199ba
jiweiqi10 <jiweiqi10@163.com>
2020-05-28 17:22:50 -0400

    update

--- update_files.sh
+++ update_files.sh
@@ -1,2 +1,4 @@
 wget -q https://raw.githubusercontent.com/youyanggu/covid19_projections/gh-pages/us-home1.html -O us-home1.html
 wget -q https://raw.githubusercontent.com/youyanggu/covid19_projections/gh-pages/us-home2.html -O us-home2.html
+wget -q https://raw.githubusercontent.com/jiweiqi/covid19-mobility/master/visulization_plotly/us_fuel_demand.html -O us_fuel_demand.html
+wget -q https://raw.githubusercontent.com/jiweiqi/covid19-mobility/master/visulization_plotly/us_map_states.html -O projection/us_map_states.html
